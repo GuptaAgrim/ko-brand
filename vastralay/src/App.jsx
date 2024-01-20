d43ffa0de02356367621a556d83e5878da688d00
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-20 19:03:56 +0530

    structure complete

--- vastralay/src/App.jsx
+++ vastralay/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Order from "./pages/order/Order";
+import NoPage from "./pages/nopage/NoPage";
 import Dashboard from "./pages/admin/dashboard/Dashboard";
 import Cart from "./pages/cart/Cart";
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/order" element={<Order />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/*" element={<NoPage />} />
       </Routes>
     </Router>
   );
