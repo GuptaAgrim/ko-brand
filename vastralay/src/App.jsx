178ac3f0f438640291aad708251d82481296fec6
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-21 20:16:55 +0530

    Front_end_done

--- vastralay/src/App.jsx
+++ vastralay/src/App.jsx
@@ -8,6 +8,8 @@ import MyState from "./context/data/myState";
 import Login from "./pages/registration/Login";
 import Signup from "./pages/registration/Signup";
 import ProductInfo from "./pages/productInfo/ProductInfo";
+import AddProduct from "./pages/admin/pages/AddProduct";
+import UpdateProduct from "./pages/admin/pages/UpdateProduct";
 
 function App() {
   return (
@@ -19,7 +21,10 @@ function App() {
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/login" element={< Login/>} />
+        <Route path="/addProduct" element={<AddProduct/>}/>
+        <Route path="/updateProduct" element={<UpdateProduct/>}/>
         <Route path="/signup" element={<Signup />} />
         <Route path='/productinfo/:id' element={<ProductInfo />} />
         <Route path="/*" element={<NoPage />} />
