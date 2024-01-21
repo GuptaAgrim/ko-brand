638be3f7c0d0cd22f8977ce8ca49aaab565f9b20
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-21 15:19:25 +0530

    Hero_section_added

---
+++ vastralay/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+      <img src="https://wallpapercave.com/wp/wp3537552.jpg" alt='error'/>
+    </div>
+  )
+}
+
+export default HeroSection
