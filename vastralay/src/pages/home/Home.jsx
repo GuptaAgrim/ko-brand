8b1d647751dbacfdefa0679d6d00081b96f117de
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-21 16:39:05 +0530

    testimonial

--- vastralay/src/pages/home/Home.jsx
+++ vastralay/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import myContext from "../../context/data/myContext";
 import HeroSection from "../../components/heroSection/HeroSection";
 import Filter from "../../components/filter/Filter";
 import ProductCard from "../../components/productCard/ProductCard";
+import Testimonial from "../../components/testimonial/Testimonial";
 
 function Home() {
   
@@ -12,6 +13,7 @@ function Home() {
     <HeroSection/>
     <Filter/>
     <ProductCard/>
+    <Testimonial/>
     </Layout>
 
 )
