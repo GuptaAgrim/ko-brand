8b45d6a378f6f4459083cea5b37397875374c306
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-23 01:32:22 +0530

    signup_page

--- vastralay/src/pages/registration/Signup.jsx
+++ vastralay/src/pages/registration/Signup.jsx
@@ -1,6 +1,49 @@
+import { useContext, useState } from 'react';
 import { Link } from 'react-router-dom'
-
+import myContext from '../../context/data/myContext';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { auth, fireDB } from '../../firebase/FirebaseConfig';
+import { Timestamp, addDoc, collection } from 'firebase/firestore';
+import { toast } from 'react-toastify';
 function Signup() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const context = useContext(myContext);
+    const { loading, setLoading } = context;
+
+    const signup = async () => {
+        setLoading(true)
+        if (name === "" || email === "" || password === "") {
+            return toast.error("All fields are required")
+        }
+
+        try {
+            const users = await createUserWithEmailAndPassword(auth, email, password);
+
+            // console.log(users)
+
+            const user = {
+                name: name,
+                uid: users.user.uid,
+                email: users.user.email,
+                time : Timestamp.now()
+            }
+            const userRef = collection(fireDB, "users")
+            await addDoc(userRef, user);
+            toast.success("Signup Succesfully")
+            setName("");
+            setEmail("");
+            setPassword("");
+            setLoading(false)
+            
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
    
     return (
         <div className=' flex justify-center items-center h-screen'>
@@ -8,24 +51,39 @@ function Signup() {
                 <div className="">
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
                 </div>
+
+                <div>
+                    <input type="text"
+                        name='name'
+                        value={name}
+                        onChange={(e)=>setName(e.target.value)}
+                        className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
+                        placeholder='Name'
+                    />
+                </div>
                 <div>
                     <input type="email"
                         name='email'
+                        value={email}
+                        onChange={(e)=>setEmail(e.target.value)}
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Email'
                     />
                 </div>
                 <div>
-                    <input
-                        type="password"
+                    <input type="password"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value)}
+                        
                         className=' bg-gray-600 mb-4 px-2 py-2 w-full lg:w-[20em] rounded-lg text-white placeholder:text-gray-200 outline-none'
                         placeholder='Password'
                     />
                 </div>
                 <div className=' flex justify-center mb-3'>
                     <button
+                    onClick={signup}
                         className=' bg-green-500 w-full text-black font-bold  px-2 py-2 rounded-lg'>
-                        Signup
+                        { loading?"loading....":"signup"}
                     </button>
                 </div>
                 <div>
