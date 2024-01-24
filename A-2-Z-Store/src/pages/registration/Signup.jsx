e177b67696f510386d0098789690736b42cf68d1
Agrim Gupta <agrimgupta359@gmail.com>
2024-01-24 18:48:52 +0530

    protected_routs

--- A-2-Z-Store/src/pages/registration/Signup.jsx
+++ A-2-Z-Store/src/pages/registration/Signup.jsx
@@ -5,6 +5,7 @@ import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth, fireDB } from '../../firebase/FirebaseConfig';
 import { Timestamp, addDoc, collection } from 'firebase/firestore';
 import { toast } from 'react-toastify';
+import Loader from '../../components/loader/Loader';
 function Signup() {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
@@ -47,6 +48,7 @@ function Signup() {
    
     return (
         <div className=' flex justify-center items-center h-screen'>
+           {loading && <Loader/>}
             <div className=' bg-gray-800 px-10 py-10 rounded-xl '>
                 <div className="">
                     <h1 className='text-center text-white text-xl mb-4 font-bold'>Signup</h1>
