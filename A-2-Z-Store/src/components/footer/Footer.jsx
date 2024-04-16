760bf713c1336d2310bfefa87a4997c7fa553581
Agrim Gupta <agrimgupta359@gmail.com>
2024-04-17 01:06:02 +0530

    mail change

--- A-2-Z-Store/src/components/footer/Footer.jsx
+++ A-2-Z-Store/src/components/footer/Footer.jsx
@@ -64,7 +64,7 @@ function Footer() {
                         </div>
                     </Link>
                     <p className="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4" style={{ color: mode === 'dark' ? 'white' : '' }}>© 2023 A-2-Z-Store —
-                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank" style={{ color: mode === 'dark' ? 'white' : '' }}>www.ebharat.com</a>
+                        <a href="https://twitter.com/knyttneve" rel="noopener noreferrer" className="text-gray-600 ml-1" target="_blank" style={{ color: mode === 'dark' ? 'white' : '' }}>www.A-2-Z-Store.com</a>
                     </p>
                     <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
                         <a className="text-gray-500">
